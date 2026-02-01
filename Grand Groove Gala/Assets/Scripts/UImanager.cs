f161b863adb63b598c42f2b8f7fb34642ce78ab0
Stitchman2000 <catfishskywalker@gmail.com>
2026-02-01 11:35:20 -0800

    added arrows

---
+++ Grand Groove Gala/Assets/Scripts/UImanager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+
+
+    public RawImage[] ArrowArray;
+
+    PlayerMovementPrime PlayerRef;
+
+    public RawImage MyImage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        PlayerRef = gameObject.GetComponent<PlayerMovementPrime>();
+
+
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (PlayerRef.DirectionToMoveX == "Up") { MyImage = ArrowArray[0].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[0].GetComponent<RawImage>().color = Color.white; }
+
+        if (PlayerRef.DirectionToMoveX == "Down") { MyImage = ArrowArray[4].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[4].GetComponent<RawImage>().color = Color.white; }
+
+        if (PlayerRef.DirectionToMoveY == "Left") { MyImage = ArrowArray[2].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[2].GetComponent<RawImage>().color = Color.white; }
+
+        if (PlayerRef.DirectionToMoveY == "Right") { MyImage = ArrowArray[6].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[6].GetComponent<RawImage>().color = Color.white; }
+
+        if ((PlayerRef.DirectionToMoveY == "Left") && (PlayerRef.DirectionToMoveX == "Up")) { MyImage = ArrowArray[7].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[7].GetComponent<RawImage>().color = Color.white; }
+
+        if ((PlayerRef.DirectionToMoveY == "Right") && (PlayerRef.DirectionToMoveX == "Up")) { MyImage = ArrowArray[1].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[1].GetComponent<RawImage>().color = Color.white; }
+
+        if ((PlayerRef.DirectionToMoveY == "Left") && (PlayerRef.DirectionToMoveX == "Down")) { MyImage = ArrowArray[5].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[5].GetComponent<RawImage>().color = Color.white; }
+
+        if ((PlayerRef.DirectionToMoveX == "Down") && (PlayerRef.DirectionToMoveY == "Right")) { MyImage = ArrowArray[3].GetComponent<RawImage>(); MyImage.color = Color.red; } else { ArrowArray[3].GetComponent<RawImage>().color = Color.white; }
+
+    }
+
+}
