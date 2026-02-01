c21f81b6bc65d132cb587927c1d400287955b9c0
Stitchman2000 <catfishskywalker@gmail.com>
2026-02-01 01:29:48 -0800

    eyeball works

--- Grand Groove Gala/Assets/Scripts/HealthComponent.cs
+++ Grand Groove Gala/Assets/Scripts/HealthComponent.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthComponent : MonoBehaviour
 {
     public float maxHealth = 10f;
     private float currentHealth;
+    public Slider Healthbar;
+
+
     private void Awake()
     {
         currentHealth = maxHealth;
     }
+
+    public void SetMaxHealth()
+    {
+
+        Healthbar.maxValue = maxHealth;
+        Healthbar.value = currentHealth;
+
+    }
+
+    public void SetHealth()
+    {
+
+        Healthbar.value = currentHealth;
+
+    }
+
 
     private void Update()
     {
@@ -20,6 +40,7 @@ public class HealthComponent : MonoBehaviour
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
+        SetHealth();
         if (currentHealth <= 0)
         {
             Death();
